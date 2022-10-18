15098d640106bf81208070496087c5697e8f42cf
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-10-18 18:39:07 +0300

    In one function. Checking init massive.

--- scripts/slider.js
+++ scripts/slider.js
@@ -28,110 +28,122 @@ const repairingInfo = [
   },
 ];
 
-const images = document.querySelector(".project-image");
-const mainNavigation = document.querySelector(".projects-menu-list");
-const repairingDescription = document.querySelector(".project-detailes-list");
-const dots = document.querySelector(".additional-menu-list");
-const buttons = document.querySelectorAll(".project-button");
+const showRepairingInfo = (repairingInfo) => {
 
-let activeProjectInfo = 0;
+    if(!repairingInfo || !repairingInfo.length) {
+        return;
+    }
 
-const getInfo = () => {
-  repairingInfo.forEach((el, index) => {
-    let image = 
-        `<img 
-            class="${index === 0 ? "" : "hidden"}" 
-            data-index="${index}" 
-            src="${el.scr}" 
-            alt="${el.alt}">`;
-    images.innerHTML += image;
-    let detailes = 
-        `<div class="project-detailes-item ${index === 0 ? "" : "hidden"}" data-index="${index}"> 
-            <div class="city-detailes">
-                <h3>City:</h3>
-                <span>${el.cityDetailes}</span>
-            </div>
-            <div class="apartment-area-detailes">
-                <h3>apartment area:</h3>
-                <span>${el.apartmentAreaDetailes}</span>
-            </div>
-            <div class="repair-time-detailes">
-                <h3>Repair time:</h3>
-                <span>${el.repairTimeDetailes}</span>
-            </div>
-            <div class="repair-cost-detailes">
-                <h3>Repair Cost:</h3>
-                <span>${el.repairCostDetailes}</span>
-            </div>
-        </div>`;
-    repairingDescription.innerHTML += detailes;
-    let menuItem = 
-        `<li class="projects-menu-item" data-index="${index}">
-            <a class="${index === 0 ? "active-link" : ""}" href="#">${el.projectsMenuItem}</a>
-        </li>`;
-    mainNavigation.innerHTML += menuItem;
-  });
-};
+    const images = document.querySelector(".project-image");
+    const mainNavigation = document.querySelector(".projects-menu-list");
+    const repairingDescription = document.querySelector(".project-detailes-list");
+    const dots = document.querySelector(".additional-menu-list");
+    const buttons = document.querySelectorAll(".project-button");
 
-const getDots = () => {
-  const dotsNumber = repairingInfo.length;
-  for (let i = 0; i < dotsNumber; i++) {
-    let dot = 
-        `<li class="additional-menu-item ${i === 0 ? "active-dot" : ""}">
-            <div></div>
-        </li>`;
-    dots.innerHTML += dot;
-  }
-};
+    let activeProjectInfo = 0;
 
-const hideProject = () => {
-    images.childNodes[activeProjectInfo].classList.add("hidden");
-    repairingDescription.childNodes[activeProjectInfo].classList.add("hidden");
-    mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.remove("active-link");
-    dots.childNodes[activeProjectInfo].classList.remove("active-dot");
-}
+    const getInfo = () => {
+    repairingInfo.forEach((el, index) => {
+        let image = 
+            `<img 
+                class="${index === 0 ? "" : "hidden"}" 
+                data-index="${index}" 
+                src="${el.scr}" 
+                alt="${el.alt}">`;
+        images.innerHTML += image;
+        let detailes = 
+            `<div class="project-detailes-item ${index === 0 ? "" : "hidden"}" data-index="${index}"> 
+                <div class="city-detailes">
+                    <h3>City:</h3>
+                    <span>${el.cityDetailes}</span>
+                </div>
+                <div class="apartment-area-detailes">
+                    <h3>apartment area:</h3>
+                    <span>${el.apartmentAreaDetailes}</span>
+                </div>
+                <div class="repair-time-detailes">
+                    <h3>Repair time:</h3>
+                    <span>${el.repairTimeDetailes}</span>
+                </div>
+                <div class="repair-cost-detailes">
+                    <h3>Repair Cost:</h3>
+                    <span>${el.repairCostDetailes}</span>
+                </div>
+            </div>`;
+        repairingDescription.innerHTML += detailes;
+        let menuItem = 
+            `<li class="projects-menu-item" data-index="${index}">
+                <a class="${index === 0 ? "active-link" : ""}" href="#">${el.projectsMenuItem}</a>
+            </li>`;
+        mainNavigation.innerHTML += menuItem;
+    });
+    };
 
-const showProject = () => {
-    images.childNodes[activeProjectInfo].classList.remove("hidden");
-    repairingDescription.childNodes[activeProjectInfo].classList.remove("hidden");
-    mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.add("active-link");
-    dots.childNodes[activeProjectInfo].classList.add("active-dot");
-}
+    const getDots = () => {
+    const dotsNumber = repairingInfo.length;
+    for (let i = 0; i < dotsNumber; i++) {
+        let dot = 
+            `<li class="additional-menu-item ${i === 0 ? "active-dot" : ""}">
+                <div></div>
+            </li>`;
+        dots.innerHTML += dot;
+    }
+    };
 
-const setButtonsActions = () => {
-    buttons.forEach(el => {
-        el.addEventListener("click", event => {
-            event.preventDefault();
-            hideProject();
-            if(el.classList.contains("right")) {
-                activeProjectInfo++;
-                if(activeProjectInfo > (repairingInfo.length-1)) {
-                    activeProjectInfo = 0;
+    const hideProject = () => {
+        images.childNodes[activeProjectInfo].classList.add("hidden");
+        repairingDescription.childNodes[activeProjectInfo].classList.add("hidden");
+        mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.remove("active-link");
+        dots.childNodes[activeProjectInfo].classList.remove("active-dot");
+    }
+
+    const showProject = () => {
+        images.childNodes[activeProjectInfo].classList.remove("hidden");
+        repairingDescription.childNodes[activeProjectInfo].classList.remove("hidden");
+        mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.add("active-link");
+        dots.childNodes[activeProjectInfo].classList.add("active-dot");
+    }
+
+    const setButtonsActions = () => {
+        buttons.forEach(el => {
+            el.addEventListener("click", event => {
+                event.preventDefault();
+                hideProject();
+                if(el.classList.contains("right")) {
+                    activeProjectInfo++;
+                    if(activeProjectInfo > (repairingInfo.length-1)) {
+                        activeProjectInfo = 0;
+                    }
+                } else {
+                    activeProjectInfo--;
+                    if(activeProjectInfo < 0) {
+                        activeProjectInfo = repairingInfo.length-1;
+                    }
                 }
-            } else {
-                activeProjectInfo--;
-                if(activeProjectInfo < 0) {
-                    activeProjectInfo = repairingInfo.length-1;
-                }
-            }
-            showProject();
-        }) 
-    })
-}
-
-const setitemsActions = (className) => {
-    const items = document.querySelectorAll(className);
-    items.forEach ((el, index) => {
-        el.addEventListener("click", () => {
-            hideProject();
-            activeProjectInfo = index;
-            showProject();
+                showProject();
+            }) 
         })
-    })
+    }
+
+    const setitemsActions = (className) => {
+        const items = document.querySelectorAll(className);
+        items.forEach ((el, index) => {
+            el.addEventListener("click", event => {
+                event.preventDefault();
+                hideProject();
+                activeProjectInfo = index;
+                showProject();
+            })
+        })
+    }
+
+    getInfo();
+    getDots();
+    setButtonsActions();
+    setitemsActions(".additional-menu-item");
+    setitemsActions(".projects-menu-item");
 }
 
-getInfo();
-getDots();
-setButtonsActions();
-setitemsActions(".additional-menu-item");
-setitemsActions(".projects-menu-item");
+document.addEventListener("DOMContentLoaded", () => {
+    showRepairingInfo(repairingInfo);
+});
