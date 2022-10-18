039b1ffa0e34cee15916242077ac284f35ee1c2c
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-10-18 17:11:17 +0300

    Buttons function optimization.

--- scripts/slider.js
+++ scripts/slider.js
@@ -32,10 +32,7 @@ const images = document.querySelector(".project-image");
 const mainNavigation = document.querySelector(".projects-menu-list");
 const repairingDescription = document.querySelector(".project-detailes-list");
 const dots = document.querySelector(".additional-menu-list");
-const leftArrow = document.querySelector(".button-left");
-const rightArrow = document.querySelector(".button-right");
-const leftButton = document.querySelector(".projects-previous-button");
-const rightButton = document.querySelector(".projects-next-button");
+const buttons = document.querySelectorAll(".project-button");
 
 let activeProjectInfo = 0;
 
@@ -87,37 +84,33 @@ const getDots = () => {
   }
 };
 
+const setButtonActions = () => {
+    buttons.forEach(el => {
+        el.addEventListener("click", event => {
+            event.preventDefault();
+            images.childNodes[activeProjectInfo].classList.add("hidden");
+            repairingDescription.childNodes[activeProjectInfo].classList.add("hidden");
+            mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.remove("active-link");
+            dots.childNodes[activeProjectInfo].classList.remove("active-dot");
+            if(el.classList.contains("right")) {
+                activeProjectInfo++;
+                if(activeProjectInfo > (repairingInfo.length-1)) {
+                    activeProjectInfo = 0;
+                }
+            } else {
+                activeProjectInfo--;
+                if(activeProjectInfo < 0) {
+                    activeProjectInfo = repairingInfo.length-1;
+                }
+            }
+            images.childNodes[activeProjectInfo].classList.remove("hidden");
+            repairingDescription.childNodes[activeProjectInfo].classList.remove("hidden");
+            mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.add("active-link");
+            dots.childNodes[activeProjectInfo].classList.add("active-dot");
+        }) 
+    })
+}
+
 getInfo();
 getDots();
-
-rightArrow.addEventListener("click", (event) => {
-    event.preventDefault();
-    images.childNodes[activeProjectInfo].classList.add("hidden");
-    repairingDescription.childNodes[activeProjectInfo].classList.add("hidden");
-    mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.remove("active-link");
-    dots.childNodes[activeProjectInfo].classList.remove("active-dot");
-    activeProjectInfo++;
-    if(activeProjectInfo > (repairingInfo.length-1)) {
-        activeProjectInfo = 0;
-    }
-    images.childNodes[activeProjectInfo].classList.remove("hidden");
-    repairingDescription.childNodes[activeProjectInfo].classList.remove("hidden");
-    mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.add("active-link");
-    dots.childNodes[activeProjectInfo].classList.add("active-dot");
-})
-
-leftArrow.addEventListener("click", (event) => {
-    event.preventDefault();
-    images.childNodes[activeProjectInfo].classList.add("hidden");
-    repairingDescription.childNodes[activeProjectInfo].classList.add("hidden");
-    mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.remove("active-link");
-    dots.childNodes[activeProjectInfo].classList.remove("active-dot");
-    activeProjectInfo--;
-    if(activeProjectInfo < 0) {
-        activeProjectInfo = repairingInfo.length-1;
-    }
-    images.childNodes[activeProjectInfo].classList.remove("hidden");
-    repairingDescription.childNodes[activeProjectInfo].classList.remove("hidden");
-    mainNavigation.childNodes[activeProjectInfo].childNodes[1].classList.add("active-link");
-    dots.childNodes[activeProjectInfo].classList.add("active-dot");
-})
+setButtonActions();
